41f559422116e6ef3c90ef7549e372b610ed5251
Andreas Hartmann <hartan@7x.de>
2022-02-24 09:00:14 +0100

    watch-serve.sh: Add container support
    for people that have a container runtime available and prefer not to
    install the dependencies locally. If the dependencies are installed
    locally, it will call these directly instead of using the container.
    Otherwise it will check if podman is installed and use that, or docker
    otherwise, build the container from the `Containerfile` and run it with
    the project directory mounted.
    
    If neither the dependencies nor a suitable container runtime can be
    found, a messages is printed to stdout that tells the user what is
    needed to use the script.

--- watch-serve.sh
+++ watch-serve.sh
@@ -1,3 +1,42 @@
 #!/usr/bin/env bash
 
-{ mdbook watch docs/ -d ../static/documentation & hugo server; }
+# Return 0 if command $1 exists, 1 otherwise.
+function _exists {
+    command -v "$1" >/dev/null
+}
+
+if [[ $(_exists mdbook) && $(_exists hugo) ]]; then
+    { mdbook watch docs/ -d ../static/documentation & hugo server; }
+    exit 0
+fi
+
+# Some pre-requisites are missing, try to use containers
+CRT=""
+CONTAINER_NAME="zellij-docs:latest"
+
+if $(_exists podman); then
+    CRT="$(which podman)"
+    echo "Using '$CRT' as container runtime"
+
+    $CRT build --tag "$CONTAINER_NAME" -f Containerfile
+    $CRT run --userns keep-id --user "$(id -u):$(id -g)" -v "$PWD:$PWD:z" -w "$PWD" -p 1313:1313 $CONTAINER_NAME
+
+elif $(_exists docker); then
+    CRT="$(which docker)"
+    echo "Using '$CRT' as container runtime"
+
+    $CRT build --tag "$CONTAINER_NAME" <Containerfile
+    $CRT run --user "$(id -u):$(id -g)" -v "$PWD:$PWD" -w "$PWD" -p 1313:1313 $CONTAINER_NAME
+
+else
+    echo "You must have installed either of:"
+    echo ""
+    echo "  - docker"
+    echo "  - podman"
+    echo "  - mdbook AND hugo"
+    echo ""
+    echo "To compile and preview the docs locally."
+    exit 1
+fi
+
+exit 0
