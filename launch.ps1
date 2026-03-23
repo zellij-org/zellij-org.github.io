6283a913e56da8c9b2af902b99e6f8357d611588
imsnif <imsnif@users.noreply.github.com>
2026-03-23 08:53:02 +0000

    deploy: 5e7f9978cac7ce299170ced9d73d0ed91d1db3ee

---
+++ launch.ps1
@@ -0,0 +1,44 @@
+$dir = Join-Path $env:TEMP "zellij\bootstrap"
+New-Item -ItemType Directory -Path $dir -Force | Out-Null
+
+$binary = Join-Path $dir "zellij.exe"
+
+if (Test-Path $binary) {
+    & $binary @args
+    exit $LASTEXITCODE
+}
+
+switch ($env:PROCESSOR_ARCHITECTURE) {
+    "AMD64" { $arch = "x86_64" }
+    default {
+        Write-Host "Unsupported cpu arch: $env:PROCESSOR_ARCHITECTURE"
+        exit 2
+    }
+}
+
+$url = "https://github.com/zellij-org/zellij/releases/latest/download/zellij-$arch-pc-windows-msvc.zip"
+$zipPath = Join-Path $dir "zellij.zip"
+
+try {
+    Invoke-WebRequest -Uri $url -OutFile $zipPath -UseBasicParsing
+} catch {
+    Write-Host ""
+    Write-Host "Download failed, cannot launch zellij :("
+    Write-Host "One probable cause is that a new release just happened and the binary is currently building."
+    Write-Host "Maybe try again later? :)"
+    exit 1
+}
+
+try {
+    Expand-Archive -Path $zipPath -DestinationPath $dir -Force
+    Remove-Item $zipPath
+} catch {
+    Write-Host ""
+    Write-Host "Extracting binary failed, cannot launch zellij :("
+    Write-Host "One probable cause is that a new release just happened and the binary is currently building."
+    Write-Host "Maybe try again later? :)"
+    exit 1
+}
+
+& $binary @args
+exit $LASTEXITCODE
